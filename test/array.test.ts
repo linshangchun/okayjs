ca1358f4e87a54b04cb204c3bb7023904e54a74d
shangchun <lshch.lshch@qq.com>
2025-07-13 23:43:57 +0800

    test: 修复测试函数

--- test/array.test.ts
+++ test/array.test.ts
@@ -5,8 +5,8 @@ describe('string', () => {
   it("arrayHas(['a', 'b'], 'string') = true", () => {
     expect(arrayHas(['a', 'b'], 'string')).toBe(true);
   });
-  it("arrayHas(['abc', 'xyz'], v => v.includes('x')) = true", () => {
-    expect(arrayHas<string>(['abc', 'xyz'], v => v.includes('x'))).toBe(true);
+  it("arrayHas(['abc', 'xyz'], arr => arr.join('').includes('x')) = true", () => {
+    expect(arrayHas<string>(['abc', 'xyz'], arr => arr.join('').includes('x'))).toBe(true);
   });
   it('arrayHas([1, 2, 3], 2) = true', () => {
     expect(arrayHas([1, 2, 3], 2)).toBe(true);
